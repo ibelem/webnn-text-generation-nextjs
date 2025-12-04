65e3b6f93a9fbd8bc5746b0f827a1b28adf87024
Belem Zhang <belem.zhang@intel.com>
2025-12-04 15:23:36 +0800

    Fix CVE-2025-55182

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    "https://10.239.115.52:3443", // Add your dev server IP and port here
+    "", // Add your dev server IP and port here
     // Add more origins if needed
   ],
   // other config options...
